1fa33b9e5fda5f85a9c25200943fc0da18860b35
Rayark-VOEZ <jiangkaiwen0911@gmail.com>
2022-01-17 10:41:38 +0800

    2022-01-17

--- src/app/module/user/user.module.ts
+++ src/app/module/user/user.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 // 通用
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 // 布局
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 // 数据录入
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
+import { UserInfoComponent } from 'src/app/component/user/user-info/user-info.component';
 import { UserLoginComponent } from 'src/app/component/user/user-login/user-login.component';
 import { UserSignupComponent } from 'src/app/component/user/user-signup/user-signup.component';
 
@@ -18,6 +22,7 @@ import { UserRoutingModule } from './user.routing.module';
 
 @NgModule({
     declarations: [
+        UserInfoComponent,
         UserLoginComponent,
         UserSignupComponent
     ],
@@ -26,10 +31,13 @@ import { UserRoutingModule } from './user.routing.module';
         UserRoutingModule,
         NzButtonModule,
         NzIconModule,
+        NzTypographyModule,
+        NzDividerModule,
         NzLayoutModule,
         NzDatePickerModule,
         NzFormModule,
-        NzInputModule
+        NzInputModule,
+        NzSelectModule
     ]
 })
 export class UserModule { }
